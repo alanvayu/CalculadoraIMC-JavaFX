a749504cec967fe52c2d4e2639b8d204cb20efde
alanvayu <alanvayu@gmail.com>
2026-07-24 14:30:51 -0300

    Calculadora de IMC em JavafX

---
+++ ProjetoCalculadoraIMC.java
@@ -0,0 +1,76 @@
+import javafx.application.*;
+import javafx.geometry.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+
+public class ProjetoCalculadoraIMC extends Application {
+	@Override
+	public void start(Stage palco) {
+		//Etiquetas para os campos de entrada
+		Label etiquetaPeso = new Label("Peso");
+		Label etiquetaAltura = new Label("Altura");
+
+		//Campos de texto para entrada de dados
+		TextField campoPeso = new TextField();
+		campoPeso.setPromptText("Peso em kg");
+		TextField campoAltura = new TextField();
+		campoAltura.setPromptText("Altura em metros");
+
+		//Label para mostrar o resultado do IMC
+		Label etiquetaResultado = new Label();
+
+		//Botão para calcular o IMC
+		Button botaoCalcular = new Button("Calcular IMC");
+		botaoCalcular.setOnAction(e -> {
+			try {
+				double peso = Double.parseDouble(campoPeso.getText().replace(",", "."));
+				double altura = Double.parseDouble(campoAltura.getText().replace(",", "."));
+
+				double imc = peso / (altura * altura);
+				etiquetaResultado.setText(String.format("Seu IMC é: %.2f", imc));
+			} catch (NumberFormatException ae) {
+				etiquetaResultado.setText("Por favor, insira números válidos para a altura");
+			}
+		});
+
+		//Layout vertical
+		VBox layout = new VBox(10, etiquetaPeso, campoPeso, etiquetaAltura, campoAltura,
+			botaoCalcular, etiquetaResultado);
+		layout.setPadding(new Insets(10));
+		layout.setAlignment(Pos.CENTER);
+
+		//Cena e palco
+		Scene cena = new Scene(layout, 300, 250);
+		palco.setTitle("Calculadora de IMC");
+		palco.setScene(cena);
+		palco.show();
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
+
+/* Observações de melhorias
+Tratamento de Exception
+             try {
+             	//Peso
+             	//Altura
+             	//Cálculo IMC
+             	//Resultado por ultimo
+             } catch (NumberFormatException ae) {
+             	etiquetaResultado.setText("Por favor, insira números válidos
+             	 para peso e altura.");
+             }
+
+Conversão para separação de casas decimais com virgula:
+
+//double peso = Double.parseDouble(campoPeso.getText().replace(",", "."));
+//double altura = Double.parseDouble(campoAltura.getText().replace(",", "."));
+
+*/
+
+//javac --module-path "%PATH_TO_FX%" --add-modules javafx.controls ProjetoCalculadoraIMC.java
